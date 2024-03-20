41c63fca0c5b74dd7b4c9ec4bf4a8f3ccaefb746
Oyematth01 <oyematth@gmail.com>
2024-03-20 18:21:40 +0100

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,23 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-   def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        dic = {}
+    def all(self, cls=None):
+        """Returns the list of objects of one type of class."""
         if cls:
-            dictionary = self.__objects
-            for key in dictionary:
-                partition = key.replace('.', ' ')
-                partition = shlex.split(partition)
-                if (partition[0] == cls.__name__):
-                    dic[key] = self.__objects[key]
-            return (dic)
-        else:
-            return self.__objects
-
+            return [obj for key, obj in self.__objects.items() if isinstance(obj, cls)]
+        return list(self.__objects.values())
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
