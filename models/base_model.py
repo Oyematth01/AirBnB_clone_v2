aeab195b5e4ebabaee4c351bb8c91569575c03b0
Oyematth01 <oyematth@gmail.com>
2024-03-20 18:51:49 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -8,21 +8,24 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     
+    id = ""
+    created_at = datetime.utcnow()
+    updated_at = datetime.utcnow()
+
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
             storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -34,17 +37,65 @@ class BaseModel:
         from models import storage
         self.updated_at = datetime.utcnow()
         storage.save()
+        storage.new(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary = self.__dict__.copy()
+        dictionary.pop('_sa_instance_state', None)
+        for key, value in dictionary.items():
+            if isinstance(value, datetime):
+                dictionary[key] = value.isoformat()
+        dictionary['__class__'] = type(self).__name__
         return dictionary
 
-    id = ""
-    created_at = datetime.utcnow()
-    updated_at = datetime.utcnow()
+    def delete(self):
+        """Delete the current instance from storage"""
+        from models import storage
+        storage.delete(self)
+
+class City(BaseModel):
+    """City class"""
+    __tablename__ = 'cities'
+    
+    name = ""
+    state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new city"""
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            self.name = kwargs.get('name', "")
+            self.state_id = kwargs.get('state_id', "")
+
+class State(BaseModel):
+    """State class"""
+    __tablename__ = 'states'
+    
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new state"""
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            self.name = kwargs.get('name', "")
+
+            # For DBStorage
+            if 'cities' in kwargs:
+                self.cities = kwargs['cities']
+            else:
+                self.cities = []
+
+    if storage_type == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
