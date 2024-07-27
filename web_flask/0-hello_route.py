3da445c8b5ab86982fbf2cc04145c220d90505a5
Oyematth01 <oyematth@gmail.com>
2024-07-27 14:50:08 -0700

    task1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application that displays 'Hello HBNB!' on the home page.
+"""
+
+from web_flask import app
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' on the home page."""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
---
+++ web_flask/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Initialization file for the Flask application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
